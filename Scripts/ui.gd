7328e20f90d5bf47bdc9998c7e4d5a9b16407556
ofikasty <kamilwan30@gmail.com>
2025-02-24 18:39:27 +0100

    added bones counter and "tutorial"

--- Scripts/ui.gd
+++ Scripts/ui.gd
@@ -1,13 +1,11 @@
 extends Control
-
-@onready var health_bar: ProgressBar = $PlayerInfoBox/healthBar
-@onready var health: Label = $PlayerInfoBox/healthBar/Health
-
-
+@onready var health_bar: ProgressBar = $PlayerHealth/healthBar
+@onready var health: Label = $PlayerHealth/healthBar/Health
+@onready var bones: Label = $PlayerBones/Bones
 @onready var player: CharacterBody2D = %Player
 @onready var game_menager: Node = %GameMenager
 
 func _process(delta):
 	health_bar.value = player.health
 	health.text = str(player.health) + "/100"
-	#bones_bar.text = "Kosci: " + str(game_menager.bones)
+	bones.text = "Kosci: " + str(game_menager.bones)
