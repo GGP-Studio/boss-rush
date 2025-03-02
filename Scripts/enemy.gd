3033cf3333f1dbbf4d84552c0570b08d39f9cea7
ofikasty <kamilwan30@gmail.com>
2025-03-02 13:33:15 +0100

    flashed out death of the enemy

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -3,8 +3,11 @@ extends CharacterBody2D
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @export var item_scene: PackedScene 
 func take_damage(damage: int) -> void:
+	animated_sprite.self_modulate = Color.RED
 	health -= damage
 	print(health)
+	await get_tree().create_timer(0.2).timeout  # Czeka 0.5 sekundy
+	animated_sprite.self_modulate = Color.WHITE
 	
 func _physics_process(delta: float) -> void:
 	if health <=0:
@@ -17,7 +20,8 @@ func drop_loot():
 	
 func die():
 	print(self.name," died")
-	animated_sprite.animation="death"
+	animated_sprite.play("Death")
+	await animated_sprite.animation_finished
 	set_physics_process(false)
 	drop_loot()
 	queue_free()
